547bc665b15e68dd56b22961863699ae72b656e0
Lukas Reschke <lukas@statuscode.ch>
2016-09-26 21:40:59 +0200

    Handle redirect in PHP and not in JS
    Since Nextcloud 10 we have a proper login endpoint that we can match on

--- js/preauth.js
+++
@@ -1,4 +0,0 @@
-if(typeof OC.Login !== "undefined") {
-	// Redirect to login page
-	window.location = OC.generateUrl('/apps/user_saml/saml/login')+'?requesttoken='+encodeURIComponent(OC.requestToken);
-}
