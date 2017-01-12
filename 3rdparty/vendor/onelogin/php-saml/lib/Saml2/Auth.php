9040db160eed1c02e49a5126d198176afb098749
Lukas Reschke <lukas@statuscode.ch>
2017-01-12 22:20:27 +0100

    Bump library to 2.10.3
    Signed-off-by: Lukas Reschke <lukas@statuscode.ch>

--- 3rdparty/vendor/onelogin/php-saml/lib/Saml2/Auth.php
+++ 3rdparty/vendor/onelogin/php-saml/lib/Saml2/Auth.php
@@ -81,6 +81,23 @@ class OneLogin_Saml2_Auth
     private $_lastRequestID;
 
     /**
+     * The most recently-constructed/processed XML SAML request
+     * (AuthNRequest, LogoutRequest) 
+     *
+     * @var string
+     */
+    private $_lastRequest;
+
+    /**
+     * The most recently-constructed/processed XML SAML response
+     * (SAMLResponse, LogoutResponse). If the SAMLResponse was
+     * encrypted, by default tries to return the decrypted XML 
+     *
+     * @var string
+     */
+    private $_lastResponse;
+
+    /**
      * Initializes the SP SAML instance.
      *
      * @param array|object|null $oldSettings Setting data (You can provide a OneLogin_Saml_Settings, the settings object of the Saml folder implementation)
@@ -110,7 +127,10 @@ class OneLogin_Saml2_Auth
     public function setStrict($value)
     {
         if (! (is_bool($value))) {
-            throw new Exception('Invalid value passed to setStrict()');
+            throw new OneLogin_Saml2_Error(
+                'Invalid value passed to setStrict()',
+                OneLogin_Saml2_Error::SETTINGS_INVALID_SYNTAX
+            );
         }
 
         $this->_settings->setStrict($value);
@@ -129,6 +149,7 @@ class OneLogin_Saml2_Auth
         if (isset($_POST) && isset($_POST['SAMLResponse'])) {
             // AuthnResponse -- HTTP_POST Binding
             $response = new OneLogin_Saml2_Response($this->_settings, $_POST['SAMLResponse']);
+            $this->_lastResponse = $response->getXMLDocument();
 
             if ($response->isValid($requestId)) {
                 $this->_attributes = $response->getAttributes();
@@ -168,6 +189,7 @@ class OneLogin_Saml2_Auth
         $this->_errors = array();
         if (isset($_GET) && isset($_GET['SAMLResponse'])) {
             $logoutResponse = new OneLogin_Saml2_LogoutResponse($this->_settings, $_GET['SAMLResponse']);
+            $this->_lastResponse = $logoutResponse->getXML();
             if (!$logoutResponse->isValid($requestId, $retrieveParametersFromServer)) {
                 $this->_errors[] = 'invalid_logout_response';
                 $this->_errorReason = $logoutResponse->getError();
@@ -184,6 +206,7 @@ class OneLogin_Saml2_Auth
             }
         } else if (isset($_GET) && isset($_GET['SAMLRequest'])) {
             $logoutRequest = new OneLogin_Saml2_LogoutRequest($this->_settings, $_GET['SAMLRequest']);
+            $this->_lastRequest = $logoutRequest->getXML();
             if (!$logoutRequest->isValid($retrieveParametersFromServer)) {
                 $this->_errors[] = 'invalid_logout_request';
                 $this->_errorReason = $logoutRequest->getError();
@@ -198,6 +221,8 @@ class OneLogin_Saml2_Auth
                 $inResponseTo = $logoutRequest->id;
                 $responseBuilder = new OneLogin_Saml2_LogoutResponse($this->_settings);
                 $responseBuilder->build($inResponseTo);
+                $this->_lastResponse = $responseBuilder->getXML();
+
                 $logoutResponse = $responseBuilder->getResponse();
 
                 $parameters = array('SAMLResponse' => $logoutResponse);
@@ -359,6 +384,7 @@ class OneLogin_Saml2_Auth
 
         $authnRequest = new OneLogin_Saml2_AuthnRequest($this->_settings, $forceAuthn, $isPassive, $setNameIdPolicy);
 
+        $this->_lastRequest = $authnRequest->getXML();
         $this->_lastRequestID = $authnRequest->getId();
 
         $samlRequest = $authnRequest->getRequest();
@@ -411,6 +437,7 @@ class OneLogin_Saml2_Auth
 
         $logoutRequest = new OneLogin_Saml2_LogoutRequest($this->_settings, null, $nameId, $sessionIndex, $nameIdFormat);
 
+        $this->_lastRequest = $logoutRequest->getXML();
         $this->_lastRequestID = $logoutRequest->id;
 
         $samlRequest = $logoutRequest->getRequest();
@@ -482,10 +509,11 @@ class OneLogin_Saml2_Auth
      */
     public function buildRequestSignature($samlRequest, $relayState, $signAlgorithm = XMLSecurityKey::RSA_SHA1)
     {
-        if (!$this->_settings->checkSPCerts()) {
+        $key = $this->_settings->getSPkey();
+        if (empty($key)) {
             throw new OneLogin_Saml2_Error(
-                "Trying to sign the SAML Request but can't load the SP certs",
-                OneLogin_Saml2_Error::SP_CERTS_NOT_FOUND
+                "Trying to sign the SAML Request but can't load the SP private key",
+                OneLogin_Saml2_Error::PRIVATE_KEY_NOT_FOUND
             );
         }
 
@@ -526,14 +554,13 @@ class OneLogin_Saml2_Auth
      */
     public function buildResponseSignature($samlResponse, $relayState, $signAlgorithm = XMLSecurityKey::RSA_SHA1)
     {
-        if (!$this->_settings->checkSPCerts()) {
+        $key = $this->_settings->getSPkey();
+        if (empty($key)) {
             throw new OneLogin_Saml2_Error(
-                "Trying to sign the SAML Response but can't load the SP certs",
-                OneLogin_Saml2_Error::SP_CERTS_NOT_FOUND
+                "Trying to sign the SAML Response but can't load the SP private key",
+                OneLogin_Saml2_Error::PRIVATE_KEY_NOT_FOUND
             );
         }
-
-        $key = $this->_settings->getSPkey();
 
         $objKey = new XMLSecurityKey($signAlgorithm, array('type' => 'private'));
         $objKey->loadKey($key, false);
@@ -554,5 +581,38 @@ class OneLogin_Saml2_Auth
         }
         $signature = $objKey->signData($msg);
         return base64_encode($signature);
+    }
+
+    /**
+     * Returns the most recently-constructed/processed
+     * XML SAML request (AuthNRequest, LogoutRequest)
+     *
+     * @return string The Request XML
+     */
+    public function getLastRequestXML()
+    {
+        return $this->_lastRequest;
+    }
+
+    /**
+     * Returns the most recently-constructed/processed
+     * XML SAML response (SAMLResponse, LogoutResponse).
+     * If the SAMLResponse was encrypted, by default tries
+     * to return the decrypted XML.
+     *
+     * @return string The Response XML
+     */
+    public function getLastResponseXML()
+    {
+        $response = null;
+        if (isset($this->_lastResponse)) {
+            if (is_string($this->_lastResponse)) {
+                $response = $this->_lastResponse;
+            } else {
+                $response = $this->_lastResponse->saveXML();
+            }
+        }
+        
+        return $response;
     }
 }
