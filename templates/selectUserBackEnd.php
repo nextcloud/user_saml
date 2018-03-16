7daab97aced6b02411fa1b0ea71a97c5e4e53fc7
Bjoern Schiessle <bjoern@schiessle.org>
2018-03-16 16:51:29 +0100

    add landing page to chose between SSO and direct login
    Signed-off-by: Bjoern Schiessle <bjoern@schiessle.org>

---
+++ templates/selectUserBackEnd.php
@@ -0,0 +1,20 @@
+<?php
+style('user_saml', 'selectUserBackEnd');
+
+/** @var array $_ */
+/** @var $l \OCP\IL10N */
+?>
+
+<div id="saml-select-user-back-end">
+
+<h1>Chose login option:</h1>
+
+<div class="login-option">
+	<a href="<?php p($_['directLogin']); ?>"><?php p($l->t('Direct log in')); ?></a>
+</div>
+
+<div class="login-option">
+	<a href="<?php p($_['ssoLogin']); ?>"><?php p($l->t('SSO & SAML log in')); ?></a>
+</div>
+
+</div>
