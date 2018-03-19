429e6306633f5c2ae74f751b594402303f96c5a1
Bjoern Schiessle <bjoern@schiessle.org>
2018-03-19 11:04:11 +0100

    make display name of SSO identity provider configurable
    Signed-off-by: Bjoern Schiessle <bjoern@schiessle.org>

--- lib/Settings/Admin.php
+++ lib/Settings/Admin.php
@@ -77,6 +77,11 @@ class Admin implements ISettings {
 			'lowercaseUrlencoding' =>  $this->l10n->t('ADFS URL-Encodes SAML data as lowercase, and the toolkit by default uses uppercase. Enable for ADFS compatibility on signature verification.'),
 		];
 		$generalSettings = [
+			'idp0_display_name' => [
+				'text' => $this->l10n->t('Optional display name of the identity provider (default: "SSO & SAML log in")'),
+				'type' => 'line',
+				'required' => false,
+			],
 			'uid_mapping' => [
 				'text' => $this->l10n->t('Attribute to map the UID to.'),
 				'type' => 'line',
