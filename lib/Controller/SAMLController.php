4e82b97055af202794defdada5071d03ee7cdd49
rakekniven <mark.ziegler@rakekniven.de>
2019-01-19 22:30:10 +0100

    Fixed typo.
    Reported at Transifex.
    
    Signed-off-by: Mark Ziegler <mark.ziegler@rakekniven.de>

--- lib/Controller/SAMLController.php
+++ lib/Controller/SAMLController.php
@@ -481,7 +481,7 @@ class SAMLController extends Controller {
 	 * @return Http\TemplateResponse
 	 */
 	public function base() {
-		$message = $this->l->t('This page should not be visted directly.');
+		$message = $this->l->t('This page should not be visited directly.');
 		return new Http\TemplateResponse($this->appName, 'error', ['message' => $message], 'guest');
 	}
 
