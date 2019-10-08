d64ca3457da49a78e54eb2133d92909c4ed2544a
Arthur Schiwon <blizzz@arthur-schiwon.de>
2019-10-08 18:16:00 +0200

    adjust group manager and backend
    * to db changes
    * remove prefix, i don't think we need it
    * use the query builder
    * ensure hooks are thrown on group creation
    
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>
    Signed-off-by: Jonathan Treffler <mail@jonathan-treffler.de>

--- lib/GroupManager.php
+++ lib/GroupManager.php
@@ -2,7 +2,11 @@
 
 namespace OCA\User_SAML;
 
+use OC\Hooks\PublicEmitter;
 use OCP\IDBConnection;
+use OCP\IGroupManager;
+use OCP\IUser;
+use OCP\IUserManager;
 
 class GroupManager
 {
@@ -15,167 +19,73 @@ class GroupManager
 	 * @var GroupDuplicateChecker
 	 */
 	protected $duplicateChecker;
+	/** @var IGroupManager */
+	private $groupManager;
+	/** @var IUserManager */
+	private $userManager;
+	/** @var GroupBackend */
+	private $ownGroupBackend;
 
-	public function __construct(IDBConnection $db, GroupDuplicateChecker $duplicateChecker) {
+
+	public function __construct(
+		IDBConnection $db,
+		GroupDuplicateChecker $duplicateChecker,
+		IGroupManager $groupManager,
+		IUserManager $userManager,
+		GroupBackend $ownGroupBackend
+	) {
 		$this->db = $db;
 		$this->duplicateChecker = $duplicateChecker;
-	}
-
-	/**
-	 * @return string[]
-	 */
-	public function findGroups($query = null, $limit = -1, $offset = 0) {
-		$sql = '
-			SELECT DISTINCT `group`
-			FROM `*PREFIX*user_saml_group_members`
-		';
-
-		$params = array();
-
-		if ($query !== null) {
-			$sql .= ' WHERE `group` LIKE ?';
-			$params = [
-				'%' . $query . '%'
-			];
-		}
-
-		if ($limit === -1) {
-			$limit = null;
-		}
-
-		if ($offset === 0) {
-			$offset = null;
-		}
-
-		$stmt = $this->db->prepare($sql, $limit, $offset);
-		$stmt->execute($params);
-		return $stmt->fetchAll(\PDO::FETCH_COLUMN);
-	}
-
-	/**
-	 * @return bool
-	 */
-	public function hasGroup($group) {
-		$sql = '
-			SELECT DISTINCT `group`
-			FROM `*PREFIX*user_saml_group_members`
-			WHERE `group` = ?
-		';
-
-		$stmt = $this->db->prepare($sql);
-		$stmt->execute(array($group));
-		return $stmt->rowCount() > 0;
-	}
-
-	/**
-	 * @return bool
-	 */
-	public function userInGroup($uid, $group) {
-		$sql = '
-			SELECT DISTINCT `group`
-			FROM `*PREFIX*user_saml_group_members`
-			WHERE `uid` = ? AND `group` = ?
-		';
-
-		$stmt = $this->db->prepare($sql);
-		$stmt->execute(array($uid, $group));
-		return $stmt->rowCount() > 0;
-	}
-
-	/**
-	 * @return string[]
-	 */
-	public function userGroups($uid) {
-		$sql = '
-			SELECT DISTINCT `group`
-			FROM `*PREFIX*user_saml_group_members`
-			WHERE `uid` = ?
-		';
-
-		$stmt = $this->db->prepare($sql);
-		$stmt->execute(array($uid));
-		return $stmt->fetchAll(\PDO::FETCH_COLUMN);
-	}
-
-	/**
-	 * @return string[]
-	 */
-	public function usersInGroup($gid, $query = null, $limit = -1, $offset = 0) {
-		$sql = '
-			SELECT DISTINCT `uid`
-			FROM `*PREFIX*user_saml_group_members`
-			WHERE `group` = ?
-		';
-
-		$params = array($gid);
-
-		if ($query !== null) {
-			$sql .= ' AND `uid` LIKE ?';
-			$params = [
-				'%' . $query . '%'
-			];
-		}
-
-		if ($limit === -1) {
-			$limit = null;
-		}
-
-		if ($offset === 0) {
-			$offset = null;
-		}
-
-		$stmt = $this->db->prepare($sql, $limit, $offset);
-		$stmt->execute($params);
-		return $stmt->fetchAll(\PDO::FETCH_COLUMN);
+		$this->groupManager = $groupManager;
+		$this->userManager = $userManager;
+		$this->ownGroupBackend = $ownGroupBackend;
 	}
 
 	public function replaceGroups($uid, $saml) {
-		$assgined = $this->userGroups($uid);
-		$this->removeGroups($uid, array_diff($assgined, $saml));
-		$this->addGroups($uid, array_diff($saml, $assgined));
-	}
-
-	public function removeGroup($uid, $group) {
-		$this->removeGroups($uid, array($group));
-	}
-
-	public function removeGroups($uid, $groups) {
-		if (count($groups) === 0) {
+		$user = $this->userManager->get($uid);
+		if($user === null) {
 			return;
 		}
-
-		$groups = array_values($groups);
-		$inQuery = implode(',', array_fill(0, count($groups), '?'));
-
-		$sql = '
-			DELETE FROM `*PREFIX*user_saml_group_members`
-			WHERE `uid` = ? AND `group` IN (' . $inQuery . ')
-		';
-
-		$stmt = $this->db->prepare($sql);
-		$stmt->bindParam(1, $uid);
-		foreach ($groups as $k => $id) {
-			$stmt->bindValue(($k + 2), $id);
-		}
-		return $stmt->execute();
+		$assigned = $this->groupManager->getUserGroups($uid);
+		$this->removeGroups($user, array_diff($assigned, $saml));
+		$this->addGroups($uid, array_diff($saml, $assigned));
 	}
 
-	public function addGroups($uid, $groups) {
-		foreach ($groups as $group) {
-			$this->addGroup($uid, $group);
+	public function removeGroups(IUser $user, array $groupIds) {
+		foreach ($groupIds as $gid) {
+			$this->removeGroup($user, $gid);
 		}
 	}
 
-	public function addGroup($uid, $group) {
-		$this->duplicateChecker->checkForDuplicates($group);
+	public function removeGroup(IUser $user, string $gid) {
+		$group = $this->groupManager->get($gid);
+		if($group === null) {
+			return;
+		}
+		$group->removeUser($user);
+	}
 
-		$sql = '
-			INSERT INTO `*PREFIX*user_saml_group_members`
-		    (`uid`, `group`)
-		    VALUES (?, ?)
-		';
+	public function addGroups(IUser $user, $groupIds) {
+		foreach ($groupIds as $gid) {
+			$this->addGroup($user, $gid);
+		}
+	}
 
-		$stmt = $this->db->prepare($sql);
-		return $stmt->execute(array($uid, $group));
+	public function addGroup(IUser $user, $gid) {
+		$group = $this->groupManager->get($gid);
+		if($group === null) {
+			if($this->groupManager instanceof PublicEmitter) {
+				$this->groupManager->emit('\OC\Group', 'preCreate', array($gid));
+			}
+			if(!$this->ownGroupBackend->createGroup($gid)) {
+				return;
+			}
+
+			$group = $this->groupManager->get($gid);
+			if($this->groupManager instanceof PublicEmitter) {
+				$this->groupManager->emit('\OC\Group', 'postCreate', array($group));
+			}
+		}
+		$group->addUser($user);
 	}
 }
