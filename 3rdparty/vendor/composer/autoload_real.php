a0793c0f50acc03ea1d0cb06a4ac871b955dc32c
Arthur Schiwon <blizzz@arthur-schiwon.de>
2021-02-16 11:54:07 +0100

    update composer files
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- 3rdparty/vendor/composer/autoload_real.php
+++ 3rdparty/vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInitcc75f134f7630c1ee3a8e4d7c86f3bcc
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitcc75f134f7630c1ee3a8e4d7c86f3bcc', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitcc75f134f7630c1ee3a8e4d7c86f3bcc', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitcc75f134f7630c1ee3a8e4d7c86f3bcc::getInitializer($loader));
         } else {
