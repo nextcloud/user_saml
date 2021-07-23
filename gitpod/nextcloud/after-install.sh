00fb65aa8c441b757c3e59633cb7af3884400aaf
Jonathan Treffler <mail@jonathan-treffler.de>
2021-07-23 10:02:57 +0000

    improved after_install.sh
    Signed-off-by: Jonathan Treffler <mail@jonathan-treffler.de>

--- gitpod/nextcloud/after-install.sh
+++ gitpod/nextcloud/after-install.sh
@@ -15,4 +15,8 @@ run_as "php /var/www/html/occ config:system:set debug --value='true' --type=bool
 # Weirdly the Nextcloud docker activates user_saml app automatically but doesn't run the migration steps necessary for it to work
 run_as "php /var/www/html/occ migrations:migrate user_saml"
 
+run_as "php /var/www/html/occ app:disable firstrunwizard"
+
+run_as "php /var/www/html/occ config:system:set defaultapp --value='files'"
+
 apache2-foreground
