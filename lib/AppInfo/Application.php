ee5d747c976c84d6019fa9143c8652b867386402
Carl Schwan <carl@carlschwan.eu>
2022-05-09 12:51:12 +0200

    chore: Move from app.php to app bootstrap
    Remove a bunch of deprecated stuff
    
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * SPDX-FileCopyrightText: 2016 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
@@ -7,39 +9,47 @@
 
 namespace OCA\User_SAML\AppInfo;
 
-use OCA\Files\Event\LoadAdditionalScriptsEvent;
+use OC\Security\CSRF\CsrfTokenManager;
+use OC\User\LoginException;
+use OC_User;
 use OCA\User_SAML\DavPlugin;
+use OCA\User_SAML\GroupBackend;
+use OCA\User_SAML\GroupManager;
+use OCA\User_SAML\Listener\LoadAdditionalScriptsListener;
 use OCA\User_SAML\Middleware\OnlyLoggedInMiddleware;
 use OCA\User_SAML\SAMLSettings;
+use OCA\User_SAML\UserBackend;
 use OCP\AppFramework\App;
-use OCP\AppFramework\Utility\IControllerMethodReflector;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCP\AppFramework\Http\Events\BeforeTemplateRenderedEvent;
 use OCP\EventDispatcher\IEventDispatcher;
 use OCP\IConfig;
+use OCP\IDBConnection;
+use OCP\IGroupManager;
+use OCP\IL10N;
+use OCP\IRequest;
 use OCP\ISession;
 use OCP\IURLGenerator;
+use OCP\IUserManager;
 use OCP\IUserSession;
-use OCP\SabrePluginEvent;
-use OCP\Server;
-use OCP\Util;
+use OCP\L10N\IFactory;
 use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+use Throwable;
 
-class Application extends App {
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+class Application extends App implements IBootstrap {
 	public function __construct(array $urlParams = []) {
 		parent::__construct('user_saml', $urlParams);
-		$container = $this->getContainer();
+	}
 
-		/**
-		 * Middleware
-		 */
-		$container->registerService('OnlyLoggedInMiddleware', function (ContainerInterface $c) {
-			return new OnlyLoggedInMiddleware(
-				$c->get(IControllerMethodReflector::class),
-				$c->get(IUserSession::class),
-				$c->get(IURLGenerator::class)
-			);
-		});
-
-		$container->registerService(DavPlugin::class, function (ContainerInterface $c) {
+	public function register(IRegistrationContext $context): void {
+		$context->registerMiddleware(OnlyLoggedInMiddleware::class);
+		$context->registerEventListener(BeforeTemplateRenderedEvent::class, LoadAdditionalScriptsListener::class);
+		$context->registerService(DavPlugin::class, function (ContainerInterface $c) {
 			return new DavPlugin(
 				$c->get(ISession::class),
 				$c->get(IConfig::class),
@@ -47,36 +57,164 @@ class Application extends App {
 				$c->get(SAMLSettings::class)
 			);
 		});
-
-		$container->registerMiddleWare('OnlyLoggedInMiddleware');
-		$this->timezoneHandling();
 	}
 
-	public function registerDavAuth(): void {
-		$dispatcher = Server::get(IEventDispatcher::class);
-		$dispatcher->addListener('OCA\DAV\Connector\Sabre::addPlugin', function (SabrePluginEvent $event) {
-			$event->getServer()->addPlugin(Server::get(DavPlugin::class));
-		});
-	}
+	public function boot(IBootContext $context): void {
+		try {
+			$context->injectFn(function (
+				IL10N $l10n,
+				IURLGenerator $urlGenerator,
+				IConfig $config,
+				IRequest $request,
+				IUserSession $userSession,
+				ISession $session,
+				IFactory $factory,
+				SAMLSettings $samlSettings,
+				IUserManager $userManager,
+				IDBConnection $connection,
+				LoggerInterface $logger,
+				GroupManager $groupManager,
+				IEventDispatcher $dispatcher,
+				CsrfTokenManager $csrfTokenManager,
+				bool $isCLI,
+			) {
+				$groupBackend = \OC::$server->get(GroupBackend::class);
+				\OC::$server->get(IGroupManager::class)->addBackend($groupBackend);
 
-	private function timezoneHandling(): void {
-		$userSession = Server::get(IUserSession::class);
-		$session = Server::get(ISession::class);
-		$config = Server::get(IConfig::class);
+				$samlSettings = \OC::$server->get(SAMLSettings::class);
 
-		$dispatcher = Server::get(IEventDispatcher::class);
-		$dispatcher->addListener(LoadAdditionalScriptsEvent::class, function () use ($session, $config, $userSession) {
-			if (!$userSession->isLoggedIn()) {
-				return;
-			}
+				$userBackend = \OCP\Server::get(UserBackend::class);
 
-			$user = $userSession->getUser();
-			$timezoneDB = $config->getUserValue($user->getUID(), 'core', 'timezone', '');
+				$userBackend->registerBackends($userManager->getBackends());
+				OC_User::useBackend($userBackend);
 
-			if ($timezoneDB === '' || !$session->exists('timezone')) {
-				Util::addScript('user_saml', 'vendor/jstz.min');
-				Util::addScript('user_saml', 'timezone');
-			}
-		});
+				$params = [];
+
+				// Setting up the one login config may fail, if so, do not catch the requests later.
+				switch ($config->getAppValue('user_saml', 'type')) {
+					case 'saml':
+						$type = 'saml';
+						break;
+					case 'environment-variable':
+						$type = 'environment-variable';
+						break;
+					default:
+						return;
+				}
+
+				if ($type === 'environment-variable') {
+					// We should ignore oauth2 token endpoint (oauth can send the credentials as basic auth which will fail with apache auth)
+					$uri = $request->getRequestUri();
+					if (str_ends_with($uri, '/apps/oauth/api/v1/token')) {
+						return;
+					}
+
+					try {
+						OC_User::handleApacheAuth();
+					} catch (LoginException $e) {
+						if ($request->getPathInfo() === '/apps/user_saml/saml/error') {
+							return;
+						}
+						$targetUrl = $urlGenerator->linkToRouteAbsolute(
+							'user_saml.SAML.genericError',
+							[
+								'message' => $e->getMessage()
+							]
+						);
+						header('Location: ' . $targetUrl);
+						exit();
+					}
+				}
+
+				$redirectSituation = false;
+
+				$user = $userSession->getUser();
+				if ($user !== null) {
+					$enabled = $user->isEnabled();
+					if ($enabled === false) {
+						if ($request->getPathInfo() === '/apps/user_saml/saml/error') {
+							return;
+						}
+						$targetUrl = $urlGenerator->linkToRouteAbsolute(
+							'user_saml.SAML.genericError',
+							[
+								'message' => $l10n->t('This user account is disabled, please contact your administrator.')
+							]
+						);
+						header('Location: ' . $targetUrl);
+						exit();
+					}
+				}
+
+				// All requests that are not authenticated and match against the "/login" route are
+				// redirected to the SAML login endpoint
+				if (!$isCLI &&
+					!$userSession->isLoggedIn() &&
+					($request->getPathInfo() === '/login')) {
+					try {
+						$params = $request->getParams();
+					} catch (\LogicException $e) {
+						// ignore exception when PUT is called since getParams cannot parse parameters in that case
+					}
+					if (isset($params['direct']) && ($params['direct'] === 1 || $params['direct'] === '1')) {
+						return;
+					}
+					$redirectSituation = true;
+				}
+
+				$multipleUserBackEnds = $samlSettings->allowMultipleUserBackEnds();
+				$configuredIdps = $samlSettings->getListOfIdps();
+				$showLoginOptions = $multipleUserBackEnds || count($configuredIdps) > 1;
+
+				if ($redirectSituation === true && $showLoginOptions) {
+					try {
+						$params = $request->getParams();
+					} catch (\LogicException $e) {
+						// ignore exception when PUT is called since getParams cannot parse parameters in that case
+					}
+					$redirectUrl = '';
+					if (isset($params['redirect_url'])) {
+						$redirectUrl = $params['redirect_url'];
+					}
+
+					$targetUrl = $urlGenerator->linkToRouteAbsolute(
+						'user_saml.SAML.selectUserBackEnd',
+						[
+							'redirectUrl' => $redirectUrl
+						]
+					);
+					header('Location: ' . $targetUrl);
+					exit();
+				}
+
+				if ($redirectSituation === true) {
+					try {
+						$params = $request->getParams();
+					} catch (\LogicException $e) {
+						// ignore exception when PUT is called since getParams cannot parse parameters in that case
+					}
+					$originalUrl = '';
+					if (isset($params['redirect_url'])) {
+						$originalUrl = $urlGenerator->getAbsoluteURL($params['redirect_url']);
+					}
+
+					$csrfToken = $csrfTokenManager->getToken();
+					$targetUrl = $urlGenerator->linkToRouteAbsolute(
+						'user_saml.SAML.login',
+						[
+							'requesttoken' => $csrfToken->getEncryptedValue(),
+							'originalUrl' => $originalUrl,
+							'idp' => array_keys($configuredIdps)[0] ?? '',
+						]
+					);
+					header('Location: ' . $targetUrl);
+					exit();
+				}
+			});
+		} catch (Throwable $e) {
+			\OCP\Server::get(LoggerInterface::class)->critical('Error when loading user_saml app', [
+				'exception' => $e,
+			]);
+		}
 	}
 }
