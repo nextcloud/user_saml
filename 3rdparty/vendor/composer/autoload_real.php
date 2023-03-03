229a2bb23fcf14479bbe57b1dc85a2a5289b9934
Arthur Schiwon <blizzz@arthur-schiwon.de>
2023-03-03 12:52:41 +0100

    update debs
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- 3rdparty/vendor/composer/autoload_real.php
+++ 3rdparty/vendor/composer/autoload_real.php
@@ -22,21 +22,14 @@ class ComposerAutoloaderInitcc75f134f7630c1ee3a8e4d7c86f3bcc
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitcc75f134f7630c1ee3a8e4d7c86f3bcc', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitcc75f134f7630c1ee3a8e4d7c86f3bcc', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
-
-            call_user_func(\Composer\Autoload\ComposerStaticInitcc75f134f7630c1ee3a8e4d7c86f3bcc::getInitializer($loader));
-        } else {
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
-        }
+        require __DIR__ . '/autoload_static.php';
+        call_user_func(\Composer\Autoload\ComposerStaticInitcc75f134f7630c1ee3a8e4d7c86f3bcc::getInitializer($loader));
 
         $loader->setClassMapAuthoritative(true);
         $loader->register(true);
