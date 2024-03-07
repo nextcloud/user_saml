7a5ef2feb460d0019f3b5d1b7c9b6dba8d83e697
Jarkko Lehtoranta <jarkko@bytecap.fi>
2024-03-07 12:57:38 +0200

    feat(idps): Allow to filter idps based on trusted domains
    Signed-off-by: Jarkko Lehtoranta <jarkko@bytecap.fi>

--- appinfo/app.php
+++ appinfo/app.php
@@ -111,7 +111,7 @@ if (!$cli &&
 }
 
 $multipleUserBackEnds = $samlSettings->allowMultipleUserBackEnds();
-$configuredIdps = $samlSettings->getListOfIdps();
+$configuredIdps = $samlSettings->getListOfIdps($request);
 $showLoginOptions = ($multipleUserBackEnds || count($configuredIdps) > 1) && $type === 'saml';
 
 if ($redirectSituation === true && $showLoginOptions) {
@@ -152,7 +152,7 @@ if ($redirectSituation === true) {
 		[
 			'requesttoken' => $csrfToken->getEncryptedValue(),
 			'originalUrl' => $originalUrl,
-			'idp' => array_keys($configuredIdps)[0] ?? '',
+			'idp' => array_key_first($configuredIdps) ?? '',
 		]
 	);
 	header('Location: '.$targetUrl);
