d61bcd3d6753716bd03ca4acc2b19cee4ad1fc91
Malik <3097625+dermalikmann@users.noreply.github.com>
2024-10-02 15:55:19 +0200

    Edited login test to expect UserLoggedInEvent
    Signed-off-by: Malik <3097625+dermalikmann@users.noreply.github.com>

--- tests/unit/Controller/SAMLControllerTest.php
+++ tests/unit/Controller/SAMLControllerTest.php
@@ -16,6 +16,7 @@ use OCA\User_SAML\UserData;
 use OCA\User_SAML\UserResolver;
 use OCP\AppFramework\Http\RedirectResponse;
 use OCP\AppFramework\Http\TemplateResponse;
+use OCP\EventDispatcher\IEventDispatcher;
 use OCP\IConfig;
 use OCP\IL10N;
 use OCP\IRequest;
@@ -57,6 +58,7 @@ class SAMLControllerTest extends TestCase {
 	/** @var SAMLController */
 	private $samlController;
 	private ITrustedDomainHelper|MockObject $trustedDomainController;
+	private IEventDispatcher $eventDispatcher;
 
 	protected function setUp(): void {
 		parent::setUp();
@@ -74,6 +76,7 @@ class SAMLControllerTest extends TestCase {
 		$this->userData = $this->createMock(UserData::class);
 		$this->crypto = $this->createMock(ICrypto::class);
 		$this->trustedDomainController = $this->createMock(ITrustedDomainHelper::class);
+		$this->eventDispatcher = $this->createMock(IEventDispatcher::class);
 
 		$this->l->expects($this->any())->method('t')->willReturnCallback(
 			function ($param) {
@@ -320,6 +323,11 @@ class SAMLControllerTest extends TestCase {
 			->expects($this->exactly(min(1, $autoProvision)))
 			->method('createUserIfNotExists')
 			->with('MyUid');
+
+		$this->eventDispatcher
+			->expects($this->once())
+			->method('dispatchTyped')
+			->with(new UserLoggedInEvent($user, 'MyUid', null, false));
 
 		$expected = new RedirectResponse($redirect);
 		$result = $this->samlController->login(1);
