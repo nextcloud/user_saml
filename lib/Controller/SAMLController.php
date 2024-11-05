8f0693ae3c13631b7494c5990a52cf4b072db0b0
Micke Nordin <kano@sunet.se>
2024-11-05 09:02:43 +0100

    feat(PassthroughParameters): Make it possible to pass through parameters to the SAML library
    OneLogin/Auth->login can accept an array of parameters to be passed through to the IdP.
    
    This patch makes it possible to specify an array of parameters to be passed through to the IdP in config.php.
    
    For example, you can set it like this, and if we get such a parameter in
    the request to user_saml/saml/login we will pass it on to the IdP.
      'user_saml.passthrough_parameters' => ['idp_hint'],
    
    Signed-off-by: Micke Nordin <kano@sunet.se>

--- lib/Controller/SAMLController.php
+++ lib/Controller/SAMLController.php
@@ -151,9 +151,18 @@ class SAMLController extends Controller {
 		switch ($type) {
 			case 'saml':
 				$auth = new Auth($this->samlSettings->getOneLoginSettingsArray($idp));
+				$passthroughParams = $this->config->getSystemValue('user_saml.passthrough_parameters', []);
+				$passthroughValues = [];
+				foreach ($passthroughParams as $passthroughParam) {
+					$value = (string)$this->request->getParam($passthroughParam, '');
+					if ($value !== '') {
+						$passthroughValues[$passthroughParam] = $value;
+					}
+				}
+
 
 				$returnUrl = $originalUrl ?: $this->urlGenerator->linkToRouteAbsolute('user_saml.SAML.login');
-				$ssoUrl = $auth->login($returnUrl, [], false, false, true);
+				$ssoUrl = $auth->login($returnUrl, $passthroughValues, false, false, true);
 				$response = new Http\RedirectResponse($ssoUrl);
 
 				// Small hack to make user_saml work with the loginflows
