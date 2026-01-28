2c92f187d28ea1bcfcbe03431588fd35adc1e144
Hoang Pham <hoangmaths96@gmail.com>
2026-01-28 18:04:16 +0700

    test: inline p helper for template test
    Signed-off-by: Hoang Pham <hoangmaths96@gmail.com>

--- tests/unit/Template/LoginPostTemplateTest.php
+++ tests/unit/Template/LoginPostTemplateTest.php
@@ -5,58 +5,62 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-namespace OCA\User_SAML\Tests\Template;
-
-use OCP\IL10N;
-use OCP\Util;
-use Test\TestCase;
-
-class LoginPostTemplateTest extends TestCase {
-	public function testLoginPostTemplateEscapesValues(): void {
-		if (!function_exists('p')) {
-			require_once \OC::$SERVERROOT . '/lib/private/Template/functions.php';
+namespace {
+	if (!function_exists('p')) {
+		function p($string): void {
+			print(htmlspecialchars((string)$string, ENT_QUOTES, 'UTF-8'));
 		}
+	}
+}
 
-		$l = $this->createMock(IL10N::class);
-		$l->method('t')->willReturnCallback(static fn (string $text): string => $text);
+namespace OCA\User_SAML\Tests\Template {
+	use OCP\IL10N;
+	use OCP\Util;
+	use Test\TestCase;
 
-		$_ = [
-			'ssoUrl' => 'https://example.com/" onmouseover="alert(1)',
-			'samlRequest' => '"><input name="submit">',
-			'relayState' => '"><img src=x onerror=alert(1)>',
-			'sigAlg' => '"><svg onload=alert(1)>',
-			'signature' => '"><div>sig</div>',
-			'nonce' => '" onload="alert(1)',
-		];
+	class LoginPostTemplateTest extends TestCase {
+		public function testLoginPostTemplateEscapesValues(): void {
+			$l = $this->createMock(IL10N::class);
+			$l->method('t')->willReturnCallback(static fn (string $text): string => $text);
 
-		ob_start();
-		include __DIR__ . '/../../../templates/login_post.php';
-		$output = ob_get_clean();
+			$_ = [
+				'ssoUrl' => 'https://example.com/" onmouseover="alert(1)',
+				'samlRequest' => '"><input name="submit">',
+				'relayState' => '"><img src=x onerror=alert(1)>',
+				'sigAlg' => '"><svg onload=alert(1)>',
+				'signature' => '"><div>sig</div>',
+				'nonce' => '" onload="alert(1)',
+			];
 
-		$this->assertNotFalse($output);
-		$this->assertStringContainsString(
-			'action="' . Util::sanitizeHTML($_['ssoUrl']) . '"',
-			$output
-		);
-		$this->assertStringContainsString(
-			'name="SAMLRequest" value="' . Util::sanitizeHTML($_['samlRequest']) . '"',
-			$output
-		);
-		$this->assertStringContainsString(
-			'name="RelayState" value="' . Util::sanitizeHTML($_['relayState']) . '"',
-			$output
-		);
-		$this->assertStringContainsString(
-			'name="SigAlg" value="' . Util::sanitizeHTML($_['sigAlg']) . '"',
-			$output
-		);
-		$this->assertStringContainsString(
-			'name="Signature" value="' . Util::sanitizeHTML($_['signature']) . '"',
-			$output
-		);
-		$this->assertStringContainsString(
-			'nonce="' . Util::sanitizeHTML($_['nonce']) . '"',
-			$output
-		);
+			ob_start();
+			include __DIR__ . '/../../../templates/login_post.php';
+			$output = ob_get_clean();
+
+			$this->assertNotFalse($output);
+			$this->assertStringContainsString(
+				'action="' . Util::sanitizeHTML($_['ssoUrl']) . '"',
+				$output
+			);
+			$this->assertStringContainsString(
+				'name="SAMLRequest" value="' . Util::sanitizeHTML($_['samlRequest']) . '"',
+				$output
+			);
+			$this->assertStringContainsString(
+				'name="RelayState" value="' . Util::sanitizeHTML($_['relayState']) . '"',
+				$output
+			);
+			$this->assertStringContainsString(
+				'name="SigAlg" value="' . Util::sanitizeHTML($_['sigAlg']) . '"',
+				$output
+			);
+			$this->assertStringContainsString(
+				'name="Signature" value="' . Util::sanitizeHTML($_['signature']) . '"',
+				$output
+			);
+			$this->assertStringContainsString(
+				'nonce="' . Util::sanitizeHTML($_['nonce']) . '"',
+				$output
+			);
+		}
 	}
 }
