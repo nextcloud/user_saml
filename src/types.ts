d3ccf871ce2e411ff9d374d0bdf9535176902d61
Carl Schwan <carlschwan@kde.org>
2026-03-12 23:46:59 +0100

    refactor(admin): Port frontend to typescript
    Signed-off-by: Carl Schwan <carlschwan@kde.org>

---
+++ src/types.ts
@@ -0,0 +1,46 @@
+/**
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+export interface Provider {
+	id: number | string
+	name: string
+}
+
+export interface SettingAttribute {
+	type: 'checkbox' | 'line'
+	text: string
+	required?: boolean
+	global?: boolean
+	placeholder?: string
+}
+
+export interface NameIdFormat {
+	label: string
+	selected?: boolean
+}
+
+export interface ProviderConfig {
+	general?: Record<string, string>
+	sp?: Record<string, string>
+	idp?: Record<string, string>
+	security?: Record<string, string>
+	'attribute-mapping'?: Record<string, string>
+	'user-filter'?: Record<string, string>
+}
+
+export interface DraftIdp {
+	entityId: string
+	ssoUrl: string
+	sloUrl: string
+	sloResponseUrl: string
+	x509cert: string
+	passthroughParameters: string
+}
+
+export type GlobalConfig = Record<string, string>
+export type SettingsMap = Record<string, SettingAttribute>
+export type SecurityMap = Record<string, string>
+export type SecurityGeneralMap = Record<string, string | SettingAttribute>
+export type NameIdFormatsMap = Record<string, NameIdFormat>
