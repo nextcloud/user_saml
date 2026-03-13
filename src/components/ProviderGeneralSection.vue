4eaab645d47c4bc88cc31f021eb8770676795538
Carl Schwan <carlschwan@kde.org>
2026-03-13 11:22:56 +0100

    chore(lint): Setup linting and fix all the automatic issues
    Signed-off-by: Carl Schwan <carlschwan@kde.org>

--- src/components/ProviderGeneralSection.vue
+++ src/components/ProviderGeneralSection.vue
@@ -6,7 +6,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 	<template v-for="(attribute, key) in generalSettings" :key="key">
 		<p v-if="attribute.type === 'checkbox' && !attribute.global && attribute.provider_type === '' || attribute.provider_type === type">
 			<NcCheckboxRadioSwitch
-				:checked="modelValue[key] === '1'"
+				:modelValue="modelValue[key] === '1'"
 				@update:checked="(val: boolean) => onChange(key, val ? '1' : '0')">
 				{{ attribute.text }}
 			</NcCheckboxRadioSwitch>
@@ -14,21 +14,23 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 				{{ t('user_saml', 'This feature might not work with all identity providers. Use only if your IdP specifically requires POST binding for SAML requests.') }}
 			</NcNoteCard>
 		</p>
-		<NcInputField v-else-if="attribute.type === 'line' && attribute.global === undefined"
-					  :id="'user-saml-general-' + key"
-					  :label="attribute.text"
-					  :model-value="modelValue[key] ?? ''"
-					  :required="attribute.required"
-					  @update:model-value="(val: string) => onChange(key, val)" />
+		<NcInputField
+			v-else-if="attribute.type === 'line' && attribute.global === undefined"
+			:id="'user-saml-general-' + key"
+			:label="attribute.text"
+			:modelValue="modelValue[key] ?? ''"
+			:required="attribute.required"
+			@update:modelValue="(val: string) => onChange(key, val)" />
 	</template>
 </template>
 
 <script setup lang="ts">
+import type { SettingsMap } from '../types.ts'
+
 import { translate as t } from '@nextcloud/l10n'
 import NcCheckboxRadioSwitch from '@nextcloud/vue/components/NcCheckboxRadioSwitch'
 import NcInputField from '@nextcloud/vue/components/NcInputField'
 import NcNoteCard from '@nextcloud/vue/components/NcNoteCard'
-import type { SettingsMap } from '../types.ts'
 
 const props = defineProps<{
 	generalSettings: SettingsMap
@@ -40,6 +42,11 @@ const emit = defineEmits<{
 	'field-change': [key: string, value: string]
 }>()
 
+/**
+ *
+ * @param key
+ * @param value
+ */
 function onChange(key: string, value: string): void {
 	emit('update:modelValue', { ...props.modelValue, [key]: value })
 	emit('field-change', key, value)
